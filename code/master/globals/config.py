80c8b383f856ddb33f1aa58fa6d8362a0194eeba
airiclenz <airic@airiclenz.com>
2026-06-16 18:27:31 +0200

    Bump version to 3.0.0

--- code/master/globals/config.py
+++ code/master/globals/config.py
@@ -1,6 +1,6 @@
 from packaging.version import Version
 
-version = Version("2.0.0")
+version = Version("3.0.0")
 
 DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
 DATETIME_FORMAT_NO_MILL = '%Y-%m-%dT%H:%M:%S'
